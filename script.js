ae755eae053736d5ec2e7230455e702d44cfcddb
Hunter Bell <bellhunter512@gmail.com>
2022-05-05 02:58:22 -0400

    times now load properly

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ var suffixes = [
 ]
 
 var dateEl = $("#currentDay");
+var timeContainer = $("#time-container");
 
 // this function displays the time at the top of the page
 function displayTime () {
@@ -33,4 +34,36 @@ function displayTime () {
     dateEl.text("Today is: " + months[date.getMonth()] + " " + date.getDate() + tag + ", " + date.getFullYear());
 }
 
+var createTimeBlocks = function () {
+    for (var i = 0; i < 24; i++) {
+        var timeRow = $("<div>").addClass("row");
+        var timeHour = $("<div>").addClass("hour col-lg-1");
+        var timeBlock = $("<div>").addClass("time-block col-lg-10");
+        if (i < date.getHours()) {
+            timeBlock.addClass("past");
+        } else if (i == date.getHours()) {
+            timeBlock.addClass("present");
+        } else {
+            timeBlock.addClass("future");
+        }
+        var hourText = $("<p>");
+
+        if (i == 0) {
+            hourText.text("12AM");
+        } else if (i < 12) {
+            hourText.text(i + "AM");
+        } else if (i == 12) {
+            hourText.text(i + "PM");
+        } else {
+            hourText.text((i - 12) + "PM");
+        }
+
+        timeHour.append(hourText);
+        timeRow.append(timeHour, timeBlock);
+        timeContainer.append(timeRow);
+    }
+
+}
+
 displayTime();
+createTimeBlocks();
