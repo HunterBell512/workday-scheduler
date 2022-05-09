6944b5ce62dd82a5c0268ad2f1e36c7cfed7b10d
Hunter <bellhunter512@gmail.com>
2022-05-08 21:45:53 -0400

    finished product

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var date = new Date();
 var scheduleObj;
+var storageItem;
 var months = [
     "January",
     "February",
@@ -36,17 +37,28 @@ function displayTime () {
 }
 
 var initialLoad = function () {
+    if (localStorage.getItem((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear())) {
+        storageItem = localStorage.getItem((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear())
+        console.log("item found");
+        scheduleObj = JSON.parse(storageItem);
+    } else {
+        scheduleObj = {
+            date: date.getDate(),
+            month: date.getMonth() + 1,
+            year: date.getFullYear()
+        }
+    }
+
     for (var i = 0; i < 24; i++) {
         var timeRow = $("<div>").addClass("row");
         var timeHour = $("<div>").addClass("hour col-lg-1");
-        var timeBlock = $("<div>").addClass("time-block col-lg-9").attr("data-hour-div", i);
+        var timeBlock = $("<div>").addClass("time-block col-lg-10").attr("data-hour-div", i);
         var blockEvent = $("<textarea>").css({"width": "100%", "border": "none"}).attr('data-hour', i);
-        var saveBtn = $("<button>").addClass("saveBtn col-lg-1");
+        var saveBtn = $("<button>").addClass("saveBtn col-lg-1").attr({"data-save": i, "title": "Save"});
         if (i < date.getHours()) {
             timeBlock.addClass("past");
         } else if (i == date.getHours()) {
             timeBlock.addClass("present");
-            blockEvent.text("Current Time");
         } else {
             timeBlock.addClass("future");
         }
@@ -62,46 +74,63 @@ var initialLoad = function () {
             hourText.text((i - 12) + "PM");
         }
 
+        if (scheduleObj["hour" + i]) {
+            blockEvent.text(scheduleObj["hour" + i]);
+        } else {
+            blockEvent.text("");
+        }
+
+        saveBtn.text("💾")
+
         timeHour.append(hourText);
         timeBlock.append(blockEvent);
         timeRow.append(timeHour, timeBlock, saveBtn);
         timeContainer.append(timeRow);
     }
-    scheduleObj = {
-        date: date.getDate(),
-        month: date.getMonth() + 1,
-        year: date.getFullYear()
-    }
 
-    $(".time-block").on("click", "textarea", function () {
-        $(this).trigger("focus");
+    $(".saveBtn").on("click", function () {
+        scheduleObj["hour" + $(this).data("save")] = $('[data-hour~=' + $(this).data("save") + ']').val();
+        localStorage.setItem(scheduleObj["month"] + "/" + scheduleObj["date"] + "/" + scheduleObj["year"], JSON.stringify(scheduleObj));
     })
 
-    $(".time-block").on("blur", "textarea", function () {
-        scheduleObj["hour" + $(this).data("hour")] = $(this).val().trim();
-        saveToStorage();
-    })
+    // $(".time-block").on("click", "textarea", function () {
+    //     $(this).trigger("focus");
+    // })
+
+    // $(".time-block").on("blur", "textarea", function () {
+    //     scheduleObj["hour" + $(this).data("hour")] = $(this).val().trim();
+    //     saveToStorage(scheduleObj["month"], scheduleObj["date"], scheduleObj["year"]);
+    // })
 }
 
 var loadTimes = function () {
     var loadedDate = $("#datepicker").val();
     loadedDate = loadedDate.split("/");
-
-    var dateObj = {
-        date: loadedDate[1].replace(/^0+/, ''),
-        month: loadedDate[0].replace(/^0+/, ''),
-        year: loadedDate[2] 
+    for (var i in loadedDate) {
+        loadedDate[i] = loadedDate[i].replace(/^0+/, '')
     }
-
-    console.log(dateObj);
-
-    updateTimes(dateObj);
+    
+    if (localStorage.getItem(loadedDate.join("/"))) {
+        clearObject(scheduleObj);
+        storageItem = localStorage.getItem(loadedDate.join("/"))
+        console.log("item found");
+        scheduleObj = JSON.parse(storageItem);
+    } else {
+        clearObject(scheduleObj);
+        scheduleObj["month"] = loadedDate[0];
+        scheduleObj["date"] = loadedDate[1];
+        scheduleObj["year"] = loadedDate[2];
+    }
+    console.log(scheduleObj);
+    updateTimes(scheduleObj);
 }
 
 var updateTimes = function (obj) {
     for (var i = 0; i < 24; i++) {
-        debugger;
-        var timeBlock = $('[data-hour-div~=' + i + ']')
+        var timeBlock = $('[data-hour-div~=' + i + ']');
+        var timeBlockText = $('[data-hour~=' + i + ']');
+        var text = obj["hour" + i];
+        console.log(text);
 
         timeBlock.removeClass("past present future");
 
@@ -130,46 +159,32 @@ var updateTimes = function (obj) {
         } else {
             timeBlock.addClass("past");
         }
+
+        if (obj["hour" + i]) {
+            timeBlockText.val(obj["hour" + i]);
+        } else {
+            timeBlockText.val("");
+        }
     }
 }
 
-initialLoad();
-
-var saveToStorage = function () {
-    localStorage.setItem((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear(), JSON.stringify(scheduleObj));
+var clearObject = function (obj) {
+    for (const key in obj) {
+        delete obj[key];
+      }
+      console.log(obj);
 }
 
 $(".load-btn").on("click", function() {
     loadTimes();
 })
 
-// $(".time-block").on("click", "p", function () {
-//     console.log("called")
-//     var text = $(this).text();
-//     var textInput = $("<textarea>")
-//     .css({"width": "100%", "border": "none"})
-//     .val(text);
-//     $(this).replaceWith(textInput);
-//     textInput.trigger("focus");
-// });
-
-// $(".time-block").on("blur", "textarea", function () {
-//     var text = $(this)
-//     .val()
-//     .trim();
-
-//     var timeBlockP = $("<p>")
-//     .css({"width": "100%", "border": "none"})
-//     .text(text);
-
-//     $(this).replaceWith(timeBlockP);
-// })
-
 $(function() {
     $('#datepicker').datepicker({
-      changeMonth: true,
-      changeYear: true
+        changeMonth: true,
+        changeYear: true
     });
-  });
+});
 
+initialLoad();
 displayTime();
