78985343092d34c6c4fafed579f083734af8eb04
HunterBell512 <bellhunter512@gmail.com>
2024-01-29 23:59:47 -0500

    app ready for deployemnt

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 $(function () {
+  // render the current time
+  const currentTime = dayjs();
+  let header = $('#currentDay');
+  header.text(currentTime.format('dddd, MMMM D YYYY, h:mm:ss a'));
+  header.addClass('lead');
+  Clock();
+
+  let taskData = {};
   let timeContainer = $('#time-container');
-  let currentTime = new Date();
+
+  // local storage declaration
+  if (localStorage.getItem('tasks')) {
+    taskData = JSON.parse(localStorage.getItem('tasks'));
+  }
+
   for (let i = 0; i < 24; i++) {
     let timeStamp = $('<div>');
     let timeStampInfo = $('<div>');
@@ -11,9 +24,9 @@ $(function () {
     // set up the time stamp
     timeStamp.attr('id', `hour-${i}`);
 
-    if (i < currentTime.getHours()) {
+    if (i < currentTime.hour()) {
       timeStamp.addClass('row time-block past');
-    } else if (i === currentTime.getHours()) {
+    } else if (i === currentTime.hour()) {
       timeStamp.addClass('row time-block present');
     } else {
       timeStamp.addClass('row time-block future');
@@ -34,33 +47,53 @@ $(function () {
     // set up the task element
     task.addClass('col-8 col-md-10 description');
     task.attr('rows', '3');
-
-    if (i === currentTime.getHours()) {
+    
+    if (`hour-${i}` in taskData) {
+      task.val(taskData[`hour-${i}`]);
+    } else if (i === currentTime.hour()) {
       task.val('Current Time');
     }
 
     // set up the save button element
     saveButton.addClass('btn saveBtn col-2 col-md-1');
     saveButton.attr('aria-label', 'save');
+    saveButton.attr('data-number', i);
 
     // set up the save icon
     saveIcon.addClass('fas fa-save');
     saveIcon.attr('aria-hidden', 'true');
+    saveIcon.attr('data-number', i);
 
     saveButton.append(saveIcon);
     timeStamp.append(timeStampInfo, task, saveButton);
     timeContainer.append(timeStamp);
   }
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  function Clock () {
+    setInterval(() => {
+      let time = dayjs();
+      let header = $('#currentDay');
+
+      header.text(time.format('dddd, MMMM D YYYY, h:mm:ss a'));
+      header.addClass('lead');
+    }, 1000)
+  }
+
+  function SaveToStorage (taskKey, taskVal) {
+    taskData[taskKey] = taskVal;
+    localStorage.setItem('tasks', JSON.stringify(taskData));
+  }
+
+  function ClearStorage () {
+    localStorage.removeItem('tasks');
+    location.reload();
+  }
+
+  $('.saveBtn').on('click', (event) => {
+    const target = $(event.target);
+    SaveToStorage(`hour-${target.attr('data-number')}`,
+                      $(`#hour-${target.attr('data-number')}`).find('textarea').val());
+  });
+
+  $('#clearBtn').on('click', ClearStorage);
 });
